0a2ec62d0d2e0393942a8de40e4855468bc3bb45
Laras Widiastuti <larasras@live.com>
2017-04-23 02:34:38 +0700

    Menu Fragment Reminder

---
+++ app/src/main/java/com/example/a6sigma/great4ip/FragmentMenu/ReminderActivity.java
@@ -0,0 +1,54 @@
+package com.example.a6sigma.great4ip.FragmentMenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.a6sigma.great4ip.AddReminderActivity;
+import com.example.a6sigma.great4ip.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Acer on 20/04/2017.
+ */
+
+public class ReminderActivity extends Fragment{
+    private FloatingActionButton mFab;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.content_reminder, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Reminder");
+
+        mFab = (FloatingActionButton) view.findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), AddReminderActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
