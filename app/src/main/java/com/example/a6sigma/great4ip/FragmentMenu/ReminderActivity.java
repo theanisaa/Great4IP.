faa50f347c351d3788b1ac2119486e31ff124e33
Laras Widiastuti <larasras@live.com>
2017-04-27 15:37:06 +0700

    Reminder Layout

--- app/src/main/java/com/example/a6sigma/great4ip/FragmentMenu/ReminderActivity.java
+++ app/src/main/java/com/example/a6sigma/great4ip/FragmentMenu/ReminderActivity.java
@@ -1,8 +1,10 @@
 package com.example.a6sigma.great4ip.FragmentMenu;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -18,18 +20,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.a6sigma.great4ip.AddReminderActivity;
 import com.example.a6sigma.great4ip.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.format.CalendarWeekDayFormatter;
+
+import java.util.Calendar;
 
 /**
  * Created by Acer on 20/04/2017.
  */
 
 public class ReminderActivity extends Fragment{
+    private MaterialCalendarView mCalendarView;
     private FloatingActionButton mFab;
+    private TextView mDateText;
+
+    private String mDayName, mDay, mMonth, mYear;
+    private CalendarWeekDayFormatter mDayFormatter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -42,6 +57,33 @@ public class ReminderActivity extends Fragment{
 
         getActivity().setTitle("Reminder");
 
+        mCalendarView = (MaterialCalendarView) view.findViewById(R.id.datePicker);
+        mCalendarView.setSelectedDate(CalendarDay.today());
+
+        mDateText = (TextView) view.findViewById(R.id.dateText);
+        mDayFormatter = new CalendarWeekDayFormatter();
+
+        mDayName = mDayFormatter.format(mCalendarView.getSelectedDate().getDay()).toString();
+        mDay = String.valueOf(mCalendarView.getSelectedDate().getDay());
+        mMonth = String.valueOf(mCalendarView.getSelectedDate().getMonth());
+        mYear = String.valueOf(mCalendarView.getSelectedDate().getYear());
+
+        defineMonth(mMonth);
+        mDateText.setText(mDayName+" "+mMonth+" "+mDay+", "+mYear);
+
+        mCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                mDayName = mDayFormatter.format(date.getDay()).toString();
+                mDay = String.valueOf(date.getDay());
+                mMonth = String.valueOf(date.getMonth());
+                mYear = String.valueOf(date.getYear());
+
+                defineMonth(mMonth);
+                mDateText.setText(mDayName+" "+mMonth+" "+mDay+", "+mYear);
+            }
+        });
+
         mFab = (FloatingActionButton) view.findViewById(R.id.fab);
         mFab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,5 +92,49 @@ public class ReminderActivity extends Fragment{
                 startActivity(intent);
             }
         });
+    }
+
+    public void defineMonth(String monthName){
+        int month = Integer.parseInt(monthName);
+        switch (month){
+            case 1:
+                mMonth = "January";
+                break;
+            case 2:
+                mMonth = "February";
+                break;
+            case 3:
+                mMonth = "March";
+                break;
+            case 4:
+                mMonth = "April";
+                break;
+            case 5:
+                mMonth = "May";
+                break;
+            case 6:
+                mMonth = "June";
+                break;
+            case 7:
+                mMonth = "July";
+                break;
+            case 8:
+                mMonth = "August";
+                break;
+            case 9:
+                mMonth = "September";
+                break;
+            case 10:
+                mMonth = "October";
+                break;
+            case 11:
+                mMonth = "November";
+                break;
+            case 12:
+                mMonth = "Desember";
+                break;
+            default:
+                break;
+        }
     }
 }
